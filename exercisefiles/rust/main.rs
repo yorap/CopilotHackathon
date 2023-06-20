c49a67a3061352b9ba75e642be7a79a065b50e91
Reto Wehrli <reto.wehrli@bluewin.ch>
2023-06-20 17:19:25 +0000

    CopilotHackathon

--- exercisefiles/rust/main.rs
+++ exercisefiles/rust/main.rs
@@ -1,2 +1,22 @@
-// create a rust app
- 
+// create a rust demo app
+fn main() {
+    println!("Hello, world!");
+}
+
+// add method to validate swiss phonenumbers
+fn validate_phonenumber(number: &str) -> bool {
+    let mut result = false;
+    if number.len() == 12 {
+        let mut chars = number.chars();
+        if chars.next() == Some('+') {
+            if chars.next() ==  Some('4') {
+                if chars.next() == Some('1') {
+                    if chars.n  ext() == Some(' ') {
+
+                    }
+                }
+            }
+        }
+    }
+    result
+}
